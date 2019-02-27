6072575e66de108d75d77ee68f2a8f71a854ca32
SuperRonan <ro.cailleau@gmail.com>
2019-02-27 11:24:36 +0100

    [CREATING PROJECT]
    first commit
    
    adding:
    	new file:   CMakeLists.txt
    	new file:   MDI_CAILLEAU_MARIE/CMakeLists.txt
    	new file:   MDI_CAILLEAU_MARIE/MDI_CAILLEAU_MARIE.cpp
    	new file:   MDI_CAILLEAU_MARIE/MDI_CAILLEAU_MARIE.h

---
+++ MDI_CAILLEAU_MARIE/MDI_CAILLEAU_MARIE.cpp
@@ -0,0 +1,16 @@
+﻿// MDI_CAILLEAU_MARIE.cpp : Defines the entry point for the application.
+//
+
+#include "MDI_CAILLEAU_MARIE.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	
+
+
+	while (1) {}
+	return 0;
+}
