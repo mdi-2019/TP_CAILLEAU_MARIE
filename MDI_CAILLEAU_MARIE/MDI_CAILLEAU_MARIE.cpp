1740d03a07215b80a73fb1070a70e5408cf488be
BobAlban <alban-marie@sfr.fr>
2019-03-05 11:22:37 +0100

    rebase the branch Alban to master

--- MDI_CAILLEAU_MARIE/MDI_CAILLEAU_MARIE.cpp
+++ MDI_CAILLEAU_MARIE/MDI_CAILLEAU_MARIE.cpp
@@ -6,23 +6,66 @@
 #include "calculator.hpp"
 using namespace std;
 
+int get_mode()
+{
+	std::cout << "Please choose the mod (0:add | 1:sub | 2:mul | 3:div)"<<std::endl;
+	int mode;
+	std::cin >> mode;
+	return mode;
+}
 
-
-
-
+float calculate()
+{
+	int mode = get_mode();
+	float res;
+	if(mode == 0)
+	{
+		float a,b;
+		std::cin >> a;
+		std::cin >> b;
+		res = a + b;
+		std::cout << a << " + " << b << " = " << res << std::endl;
+		return res;
+	}
+	else if(mode == 1)
+	{
+		float a,b;
+		std::cin >> a;
+		std::cin >> b;
+		res = a - b;
+		std::cout << a << " + " << b << " = " << res << std::endl;
+		return res;
+	}
+	else if(mode == 2)
+	{
+		float a,b;
+		std::cin >> a;
+		std::cin >> b;
+		res = a * b;
+		std::cout << a << " + " << b << " = " << res << std::endl;
+		return res;
+	}
+	else if(mode == 3)
+	{
+		float a,b;
+		std::cin >> a;
+		std::cin >> b;
+		res = a / b;
+		std::cout << a << " + " << b << " = " << res << std::endl;
+		return res;
+	}
+	else
+	{
+		std::cout << "Unvalid mode" << std::endl;
+	}
+	
+}
 
 int main()
 {
-	Calculator calc;
-	calc.__init();
-
-	while (1) 
+	float previous = 0;
+	while(true)
 	{
-		std::string line;
-		getline(std::cin, line);
-		std::cout << calc.compute(line) << std::endl;
+		previous = calculate();
 	}
-
-	
-	return 0;
 }
